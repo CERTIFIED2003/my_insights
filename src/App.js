0aebc1b9c315e5d9a7c4545989799c1c96c7d944
CERTIFIED2003 <lal.shubham027@gmail.com>
2023-01-18 16:13:34 +0530

    Testimonial Component added

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ function App() {
         <div className="space--vertical" />
         <Services />
         <Qualification />
-        <div className="space--vertical" />
         <Testimonials />
+        <div className="space--vertical" />
       </main>
     </>
   );
--- src/components/home/ScrollDown.jsx
+++ src/components/home/ScrollDown.jsx
@@ -4,9 +4,9 @@ export default function ScrollDown() {
   return (
     <div className="home__scroll">
         <a href="#about" className="home__scroll-button button--flex">
-            <img src={Scroll} width={32} height={32} className="home__scroll-mouse wheel" alt="" />
             <span className="home__scroll-name">Scroll Down</span>
-            <i className="uil uil-arrow-down home__scroll-arrow"></i>
+            <img src={Scroll} width={32} height={32} className="home__scroll-mouse wheel" alt="" />
+            {/* <i className="uil uil-arrow-down home__scroll-arrow"></i> */}
         </a>
     </div>
   )
